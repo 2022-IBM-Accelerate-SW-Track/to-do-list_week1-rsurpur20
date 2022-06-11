1734832aae5190a051b524553706a9fa9d2acd62
Roshni_Surpur <rsurpur20@choate.edu>
2022-06-11 11:40:16 -0400

    corrected imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import "./AddTodo.js";
-import "./todos.js";
+import "../component/AddTodo.js";
+import "../component/todos.js";
 import "./Home.css";
 
 class Home extends Component {
